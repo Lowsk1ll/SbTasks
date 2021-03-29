452b33fe7b692235dcf33b4ed87137a4747060fe
BEHVIK <enjoy1999@yandex.ru>
2021-03-29 09:02:04 +0300

    Initial commit

---
+++ src/tasks/task1/Runner.java
@@ -0,0 +1,42 @@
+package tasks.task1;
+
+import java.util.Scanner;
+
+public class Runner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long k = scanner.nextLong();
+
+        long currentNumber = 1;
+        long amountOfNumbersForCurrentAmountOfDigits = 9;
+        int amountOfDigits = 1;
+        long amountOfSymbols = k + 1;
+        long currentAmountOfDigit = 0;
+        while (amountOfSymbols - amountOfNumbersForCurrentAmountOfDigits > 0) {
+            amountOfSymbols -= amountOfNumbersForCurrentAmountOfDigits;
+            if (currentAmountOfDigit + amountOfNumbersForCurrentAmountOfDigits * amountOfDigits > k + 1) {
+                break;
+            }
+            currentAmountOfDigit += amountOfNumbersForCurrentAmountOfDigits * amountOfDigits;
+            amountOfNumbersForCurrentAmountOfDigits *= 10;
+            amountOfDigits++;
+
+        }
+        for (int i = 1; i < amountOfDigits; i++) {
+            currentNumber *= 10;
+        }
+
+        currentNumber += (((k) - currentAmountOfDigit) / amountOfDigits);
+        currentAmountOfDigit += ((((k) - currentAmountOfDigit) / amountOfDigits) + 1) * amountOfDigits;
+        if (currentAmountOfDigit == k + 1) {
+            System.out.println(currentNumber % 10);
+        } else {
+            while (!(currentAmountOfDigit == (k + 1))) {
+                currentAmountOfDigit--;
+                currentNumber /= 10;
+
+            }
+            System.out.println(currentNumber % 10);
+        }
+    }
+}
